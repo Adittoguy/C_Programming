e820817ef993dac28f223571bb96eadaf71a84c4
Adittoguy <adityasanap2001@gmail.com>
2025-10-30 00:23:54 +0530

    Codes of 14 with README

---
+++ Assignment/Assignment_14/Program14_4.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   :   CountFour
+//  Description     :   Used to accept number and Check how many 4's it contains
+//  Input           :   Integer
+//  Output          :   Integer
+//  Author          :   Aditya Bhaskar Sanap
+//  Date            :   30/10/2025
+//
+////////////////////////////////////////////////////////////////////////
+
+int CountFour(int iNo)
+{
+    int iCnt = 0;
+    int iDigit = 0;
+    int iCount = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+
+        if(iDigit == 4)
+        {
+            iCount++;
+        }
+
+        iNo = iNo / 10;
+    }
+    return iCount;
+}
+
+// Time complexity :- O(log n)
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function : Main
+//
+////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter Number: ");
+    scanf("%d", &iValue);
+
+    iRet = CountFour(iValue);
+
+    printf("%d", iRet);
+
+    return 0;
+}
---
+++ Assignment/Assignment_14/Program14_5.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   :   Count
+//  Description     :   Used to accept number and Check how many numbers less than 6 it contains
+//  Input           :   Integer
+//  Output          :   Integer
+//  Author          :   Aditya Bhaskar Sanap
+//  Date            :   30/10/2025
+//
+////////////////////////////////////////////////////////////////////////
+
+int Count(int iNo)
+{
+    int iCnt = 0;
+    int iDigit = 0;
+    int iCount = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+
+        if(iDigit < 6)
+        {
+            iCount++;
+        }
+
+        iNo = iNo / 10;
+    }
+    return iCount;
+}
+
+// Time complexity :- O(log n)
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function : Main
+//
+////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter Number: ");
+    scanf("%d", &iValue);
+
+    iRet = Count(iValue);
+
+    printf("%d", iRet);
+
+    return 0;
+}
