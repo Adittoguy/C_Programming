0ef94cbc235ab2d9b2242a1001700f44d1c04011
Adittoguy <adityasanap2001@gmail.com>
2025-11-20 11:07:58 +0530

    modified

--- Assignment/Assignment_01/Program1_1.c
+++ Assignment/Assignment_01/Program1_1.c
@@ -1,3 +1,9 @@
+////////////////////////////////////////////////////////////////////////
+//
+//  Required Header Files
+//
+////////////////////////////////////////////////////////////////////////
+
 #include <stdio.h>
 
 ////////////////////////////////////////////////////////////////////////
@@ -15,14 +21,14 @@ int Divide(int iNo1, int iNo2)
 {
     int iAns = 0;
 
-    if(iNo2 == 0)                       // Our Code is static so this is of no use
+    if(iNo2 == 0)                       
     {
         return -1;
     }
 
     iAns = iNo1 / iNo2;
     return iAns;
-}
+}// End of Function
 
 ////////////////////////////////////////////////////////////////////////
 //
@@ -40,4 +46,4 @@ int main()
     printf("Division is %d", iRet);
 
     return 0;
-}
+}// End of main
