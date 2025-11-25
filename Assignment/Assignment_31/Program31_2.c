2b60a25af3d32886d1af8a5d80a88efc2973f30e
Adittoguy <adityasanap2001@gmail.com>
2025-11-25 23:24:00 +0530

    codes of 31 and 32

---
+++ Assignment/Assignment_31/Program31_2.c
@@ -0,0 +1,69 @@
+////////////////////////////////////////////////////////////////////////
+//
+//  Required Header Files
+//
+////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   :   Pattern
+//  Description     :   Used to print Pattern
+//  Input           :   Integer, Integer
+//  Output          :   Pattern
+//  Author          :   Aditya Bhaskar Sanap
+//  Date            :   25/11/2025
+//
+////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = iRow; i >= 1; i--)
+    {
+        for(j= iCol; j >= 1; j--)
+        {
+            if(j <= i)
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function : Main
+//
+////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the number of Rows: ");
+    scanf("%d", &iValue1);
+
+    printf("Enter the number of Columns: ");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Test case handled by application
+//
+//  Input   :   iRow : 4    iCol : 4 
+//
+//  Output  :   *       *       *       *
+//              *       *       *
+//              *       *
+//              *
+//
+////////////////////////////////////////////////////////////////////////
